a4860ee8ac6e1db13308669b5784ee2b40f257b6
姜灏 <150haomengting@163.com>
2016-09-10 11:20:52 +0800

    update rase

---
+++ rsae.go
@@ -0,0 +1,122 @@
+package rase
+
+import (
+	"crypto"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"errors"
+
+	"github.com/tiantour/conf"
+	"github.com/tiantour/imago"
+)
+
+// RSA rsa
+var RSAE = &irsa{}
+
+type irsa struct{}
+
+//Encrypt rsa加密
+func (r *irsa) Encrypt(origdata string) (result string, err error) {
+	publicKey, err := imago.File.Read(conf.Options.RAS.PublicKey)
+	if err != nil {
+		return
+	}
+	block, _ := pem.Decode(publicKey)
+	if block == nil {
+		err = errors.New("public key error")
+		return
+	}
+	pubInterface, err := x509.ParsePKIXPublicKey(block.Bytes)
+	if err != nil {
+		return
+	}
+	pub := pubInterface.(*rsa.PublicKey)
+	resultByte, err := rsa.EncryptPKCS1v15(rand.Reader, pub, []byte(origdata))
+	if err != nil {
+		return
+	}
+	result = imago.Crypto.Base64Encode(resultByte)
+	return
+}
+
+//Decrypt rsa解密
+func (r *irsa) Decrypt(ciphertext string) (result string, err error) {
+	privateKey, err := imago.File.Read(conf.Options.RAS.PrivateKey)
+	if err != nil {
+		return
+	}
+	block, _ := pem.Decode(privateKey)
+	if block == nil {
+		err = errors.New("private key error")
+		return
+	}
+	privInterface, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+	if err != nil {
+		return
+	}
+	resultTemp, err := imago.Crypto.Base64Decode(ciphertext)
+	if err != nil {
+		return
+	}
+	resultByte, err := rsa.DecryptPKCS1v15(rand.Reader, privInterface, resultTemp)
+	if err != nil {
+		return
+	}
+	result = string(resultByte)
+	return
+}
+
+//Sign rsa签名
+func (r *irsa) Sign(origdata string) (result string, err error) {
+	privateKey, err := imago.File.Read(conf.Options.RAS.PrivateKey)
+	if err != nil {
+		return
+	}
+	block, _ := pem.Decode(privateKey)
+	if block == nil {
+		err = errors.New("private key error")
+		return
+	}
+	privInterface, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+	if err != nil {
+		return
+	}
+	digest := imago.Crypto.SHA1(origdata)
+	resultByte, err := rsa.SignPKCS1v15(rand.Reader, privInterface, crypto.SHA1, digest)
+	if err != nil {
+		return
+	}
+	result = imago.Crypto.Base64Encode(resultByte)
+	return
+}
+
+//Verify rsa签名验证
+func (r *irsa) Verify(origdata, ciphertext string) (status bool, err error) {
+	publicKey, err := imago.File.Read(conf.Options.RAS.PublicKey)
+	if err != nil {
+		return
+	}
+	block, _ := pem.Decode(publicKey)
+	if block == nil {
+		err = errors.New("public key error")
+		return
+	}
+	pubInterface, err := x509.ParsePKIXPublicKey(block.Bytes)
+	if err != nil {
+		return
+	}
+	pub := pubInterface.(*rsa.PublicKey)
+	digest := imago.Crypto.SHA1(origdata)
+	resultTemp, err := imago.Crypto.Base64Decode(ciphertext)
+	if err != nil {
+		return
+	}
+	err = rsa.VerifyPKCS1v15(pub, crypto.SHA1, digest, resultTemp)
+	if err != nil {
+		return
+	}
+	status = true
+	return
+}
