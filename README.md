4b64cdf37d0118a35056d2775ca29e1bd2bd4cc0
姜灏 <150haomengting@163.com>
2017-05-22 15:52:01 +0800

    modify struct

--- README.md
+++ README.md
@@ -1,2 +1,259 @@
 # rsae
 rsa encrypt,decrypt,sign,verify wiht go
+
+Base64Encode
+
+```
+package main
+
+import (
+	"fmt"
+
+	"github.com/tiantour/rsae"
+)
+
+func main() {
+	args := "input data"
+	result := rsae.NewRsae().Base64Encode([]byte(args))
+	fmt.Println(result)
+}
+```
+
+Base64Decode
+
+```
+package main
+
+import (
+	"fmt"
+
+	"github.com/tiantour/rsae"
+)
+
+func main() {
+	args := "input data"
+	result, err := rsae.NewRsae().Base64Decode(args)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	fmt.Println(string(result))
+}
+```
+
+MD532
+
+```
+package main
+
+import (
+	"fmt"
+
+	"github.com/tiantour/rsae"
+)
+
+func main() {
+	args := "input data"
+	result := rsae.NewRsae().Md532(args)
+	fmt.Println(result)
+}
+```
+
+MD516
+
+```
+package main
+
+import (
+	"fmt"
+
+	"github.com/tiantour/rsae"
+)
+
+func main() {
+	args := "input data"
+	result := rsae.NewRsae().Md516(args)
+	fmt.Println(result)
+}
+```
+
+SHA1
+
+```
+package main
+
+import (
+	"fmt"
+
+	"github.com/tiantour/rsae"
+)
+
+func main() {
+	args := "input data"
+	result := rsae.NewRsae().SHA1(args)
+	fmt.Println(result)
+}
+```
+
+SHA256
+
+```
+package main
+
+import (
+	"fmt"
+
+	"github.com/tiantour/rsae"
+)
+
+func main() {
+	args := "input data"
+	result := rsae.NewRsae().SHA256(args)
+	fmt.Println(result)
+}
+```
+
+HmacSha1
+
+
+```
+package main
+
+import (
+	"fmt"
+
+	"github.com/tiantour/rsae"
+)
+
+func main() {
+	publicKey := "public key"
+	privateKey := "private key"
+	result := rsae.NewRsae().HmacSha1(publicKey, privateKey)
+	fmt.Println(result)
+}
+```
+
+Pbkdf2Sha256
+
+
+```
+package main
+
+import (
+	"fmt"
+
+	"github.com/tiantour/rsae"
+)
+
+func main() {
+	data := "input date"
+	salt := "input salt"
+	iterations := 12000
+	result := rsae.NewRsae().Pbkdf2Sha256(data, salt, iterations)
+	fmt.Println(result)
+}
+```
+Encrypt
+
+```
+package main
+
+import (
+	"fmt"
+
+	"github.com/tiantour/imago"
+	"github.com/tiantour/rsae"
+)
+
+func main() {
+	origdata := "data"
+	publicPath := "public key path"
+	publicKey, err := imago.NewFile().Read(publicPath)
+	if err != nil {
+		fmt.Println("key error")
+	}
+	result, err := rsae.NewRsae().Encrypt(origdata, publicKey)
+	if err != nil {
+		fmt.Println("encrypt error")
+	}
+	fmt.Println(result)
+}
+```
+
+Decrypt
+```
+package main
+
+import (
+	"fmt"
+
+	"github.com/tiantour/imago"
+	"github.com/tiantour/rsae"
+)
+
+func main() {
+	origdata := "data"
+	privatePath := "public key path"
+	privateKey, err := imago.NewFile().Read(privatePath)
+	if err != nil {
+		fmt.Println("key error")
+	}
+	result, err := rsae.NewRsae().Decrypt(origdata, privateKey)
+	if err != nil {
+		fmt.Println("Decrypt error")
+	}
+	fmt.Println(result)
+}
+```
+Sign
+
+```
+package main
+
+import (
+	"fmt"
+
+	"github.com/tiantour/imago"
+	"github.com/tiantour/rsae"
+)
+
+func main() {
+	origdata := "data"
+	privatePath := "public key path"
+	privateKey, err := imago.NewFile().Read(privatePath)
+	if err != nil {
+		fmt.Println("key error")
+	}
+	result, err := rsae.NewRsae().Sign(origdata, privateKey)
+	if err != nil {
+		fmt.Println("Decrypt error")
+	}
+	fmt.Println(result)
+}
+```
+Verify
+
+```
+package main
+
+import (
+	"fmt"
+
+	"github.com/tiantour/imago"
+	"github.com/tiantour/rsae"
+)
+
+func main() {
+	origdata := "data"
+	ciphertext := "result"
+	publicPath := "public key path"
+	publicKey, err := imago.NewFile().Read(publicPath)
+	if err != nil {
+		fmt.Println("key error")
+	}
+	result, err := rsae.NewRsae().Verify(origdata, ciphertext, publicKey)
+	if err != nil {
+		fmt.Println("Decrypt error")
+	}
+	fmt.Println(result)
+}
+```
--- rsae.go
+++ rsae.go
@@ -2,121 +2,179 @@ package rsae
 
 import (
 	"crypto"
+	"crypto/hmac"
+	"crypto/md5"
 	"crypto/rand"
 	"crypto/rsa"
+	"crypto/sha1"
+	"crypto/sha256"
 	"crypto/x509"
+	"encoding/base64"
+	"encoding/hex"
 	"encoding/pem"
 	"errors"
+	"fmt"
 
-	"github.com/tiantour/conf"
-	"github.com/tiantour/imago"
+	"golang.org/x/crypto/pbkdf2"
 )
 
-// RSA rsa
-var RSA = &rsae{}
+// Rsae rsae
+type Rsae struct{}
 
-type rsae struct{}
+// NewRsae new rsae
+func NewRsae() *Rsae {
+	return &Rsae{}
+}
 
-// Encrypt
-func (r *rsae) Encrypt(origdata string) (result string, err error) {
-	publicKey, err := imago.File.Read(conf.Data.RSA.PublicKey)
-	if err != nil {
-		return
-	}
+// Base64Encode base64 encode
+// date 2017-05-17
+// author andy.jiang
+func (r Rsae) Base64Encode(data []byte) string {
+	return base64.StdEncoding.EncodeToString(data)
+}
+
+//Base64Decode base64 descode
+// date 2017-05-17
+// author andy.jiang
+func (r Rsae) Base64Decode(data string) ([]byte, error) {
+	return base64.StdEncoding.DecodeString(data)
+}
+
+// Md516 md5 16
+// date 2017-05-17
+// author andy.jiang
+func (r Rsae) Md516(data string) string {
+	return r.Md532(data)[8:24]
+}
+
+// Md532 md5 32
+// date 2017-05-17
+// author andy.jiang
+func (r Rsae) Md532(data string) string {
+	h := md5.New()
+	h.Write([]byte(data))
+	return hex.EncodeToString(h.Sum(nil))
+}
+
+// SHA1 sha1
+// date 2017-05-17
+// author andy.jiang
+func (r Rsae) SHA1(data string) []byte {
+	h := sha1.New()
+	h.Write([]byte(data))
+	return h.Sum(nil)
+}
+
+// SHA256 sha256
+// date 2017-05-17
+// author andy.jiang
+func (r Rsae) SHA256(data string) []byte {
+	h := sha256.New()
+	h.Write([]byte(data))
+	return h.Sum(nil)
+}
+
+// HmacSha1 hmac sha1
+// date 2017-05-17
+// author andy.jiang
+func (r Rsae) HmacSha1(publicKey, privateKey string) []byte {
+	mac := hmac.New(sha1.New, []byte(privateKey))
+	mac.Write([]byte(publicKey))
+	return mac.Sum(nil)
+}
+
+// Pbkdf2Sha256 pbkdf2 sha256
+// date 2017-05-17
+// author andy.jiang
+func (r Rsae) Pbkdf2Sha256(data, salt string, iterations int) string {
+	dk := pbkdf2.Key([]byte(data), []byte(salt), iterations, 32, sha256.New)
+	return fmt.Sprintf("pbkdf2_sha256$%d$%s$%s", iterations, salt, base64.StdEncoding.EncodeToString(dk))
+}
+
+// Encrypt rsa entrypt
+// date 2017-05-17
+// author andy.jiang
+func (r Rsae) Encrypt(origdata string, publicKey []byte) (string, error) {
 	block, _ := pem.Decode(publicKey)
 	if block == nil {
-		err = errors.New("public key error")
-		return
+		return "", errors.New("public key error")
 	}
 	pubInterface, err := x509.ParsePKIXPublicKey(block.Bytes)
 	if err != nil {
-		return
+		return "", err
 	}
 	pub := pubInterface.(*rsa.PublicKey)
-	resultByte, err := rsa.EncryptPKCS1v15(rand.Reader, pub, []byte(origdata))
+	body, err := rsa.EncryptPKCS1v15(rand.Reader, pub, []byte(origdata))
 	if err != nil {
-		return
+		return "", err
 	}
-	result = imago.Crypto.Base64Encode(resultByte)
-	return
+	return r.Base64Encode(body), nil
 }
 
-// Decrypt
-func (r *rsae) Decrypt(ciphertext string) (result string, err error) {
-	privateKey, err := imago.File.Read(conf.Data.RSA.PrivateKey)
-	if err != nil {
-		return
-	}
+// Decrypt rsa decarypt
+// date 2017-05-17
+// author andy.jiang
+func (r Rsae) Decrypt(ciphertext string, privateKey []byte) (string, error) {
 	block, _ := pem.Decode(privateKey)
 	if block == nil {
-		err = errors.New("private key error")
-		return
+		return "", errors.New("private key error")
 	}
 	privInterface, err := x509.ParsePKCS1PrivateKey(block.Bytes)
 	if err != nil {
-		return
+		return "", err
 	}
-	resultTemp, err := imago.Crypto.Base64Decode(ciphertext)
+	resultTemp, err := r.Base64Decode(ciphertext)
 	if err != nil {
-		return
+		return "", err
 	}
-	resultByte, err := rsa.DecryptPKCS1v15(rand.Reader, privInterface, resultTemp)
+	body, err := rsa.DecryptPKCS1v15(rand.Reader, privInterface, resultTemp)
 	if err != nil {
-		return
+		return "", err
 	}
-	result = string(resultByte)
-	return
+	return string(body), nil
 }
 
-// Sign
-func (r *rsae) Sign(origdata string) (result string, err error) {
-	privateKey, err := imago.File.Read(conf.Data.RSA.PrivateKey)
-	if err != nil {
-		return
-	}
+// Sign rsa sign
+// date 2017-05-17
+// author andy.jiang
+func (r Rsae) Sign(origdata string, privateKey []byte) (string, error) {
 	block, _ := pem.Decode(privateKey)
 	if block == nil {
-		err = errors.New("private key error")
-		return
+		return "", errors.New("private key error")
 	}
 	privInterface, err := x509.ParsePKCS1PrivateKey(block.Bytes)
 	if err != nil {
-		return
+		return "", err
 	}
-	digest := imago.Crypto.SHA1(origdata)
-	resultByte, err := rsa.SignPKCS1v15(rand.Reader, privInterface, crypto.SHA1, digest)
+	digest := r.SHA256(origdata)
+	body, err := rsa.SignPKCS1v15(rand.Reader, privInterface, crypto.SHA256, digest)
 	if err != nil {
-		return
+		return "", err
 	}
-	result = imago.Crypto.Base64Encode(resultByte)
-	return
+	return r.Base64Encode(body), nil
 }
 
-// Verify
-func (r *rsae) Verify(origdata, ciphertext string) (status bool, err error) {
-	publicKey, err := imago.File.Read(conf.Data.RSA.PublicKey)
-	if err != nil {
-		return
-	}
+// Verify rsa verify
+// date 2017-05-17
+// author andy.jiang
+func (r Rsae) Verify(origdata, ciphertext string, publicKey []byte) (bool, error) {
 	block, _ := pem.Decode(publicKey)
 	if block == nil {
-		err = errors.New("public key error")
-		return
+		return false, errors.New("public key error")
 	}
 	pubInterface, err := x509.ParsePKIXPublicKey(block.Bytes)
 	if err != nil {
-		return
+		return false, err
 	}
 	pub := pubInterface.(*rsa.PublicKey)
-	digest := imago.Crypto.SHA1(origdata)
-	resultTemp, err := imago.Crypto.Base64Decode(ciphertext)
+	digest := r.SHA256(origdata)
+	body, err := r.Base64Decode(ciphertext)
 	if err != nil {
-		return
+		return false, err
 	}
-	err = rsa.VerifyPKCS1v15(pub, crypto.SHA1, digest, resultTemp)
+	err = rsa.VerifyPKCS1v15(pub, crypto.SHA256, digest, body)
 	if err != nil {
-		return
+		return false, err
 	}
-	status = true
-	return
+	return true, nil
 }
